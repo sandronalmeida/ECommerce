846e48fda136497892efa3056942b33c4eb41fbc
sandronalmeida <YOUR EMAIL ADDRESSp016188@gmail.com>
2018-01-07 21:22:25 -0200

    criamoa a classe page

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <?php 
 
 require_once("vendor/autoload.php");
+use \Slim\Slim;
+use \Hcode\Page;
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new Hcode\DB\Sql();
-	$results=$sql->select("select * from tb_users");
-	echo json_encode($results);
+	//$sql = new Hcode\DB\Sql();
+	//$results=$sql->select("select * from tb_users");
+	//echo json_encode($results);
+	$page = new Page();
+	$page->setTpl("index");
 
 });
 
