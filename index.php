ef3debefa3d6746bd4a80312104c8b44aab865b5
sandronalmeida <YOUR EMAIL ADDRESSp016188@gmail.com>
2018-01-09 21:04:03 -0200

    page admin

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -14,6 +15,12 @@ $app->get('/', function() {
 	//$results=$sql->select("select * from tb_users");
 	//echo json_encode($results);
 	$page = new Page();
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+	$page = new PageAdmin();
 	$page->setTpl("index");
 
 });
